232f14e8551d60bb7bec71f1768275a7a356f4df
Jonathanjohansson24 <jonathan.nordin24@gmail.com>
2023-09-22 10:44:46 +0200

    cleaned it up and added comments

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -12,6 +12,7 @@ namespace NumbersGame
         {
             Console.WriteLine("Välkommen till nummerspelet!\nFör att börja spelet tryck på valfri tagent!");
             Console.ReadKey();
+            Console.Clear();
 
 
             NumbersGame();
@@ -27,7 +28,7 @@ namespace NumbersGame
             int numberOfGuesses = 0;
             while (numberOfGuesses < 5)
             {
-                Console.Write("Gissa ett tal:");
+                Console.Write("Gissa ett tal: ");
                 int usersGuess;
 
                 if (int.TryParse(Console.ReadLine(), out usersGuess))
@@ -35,27 +36,28 @@ namespace NumbersGame
 
                     if (usersGuess < number)
                     {
-                        Console.WriteLine("Det riktiga talet är större än ditt tal");
-                        numberOfGuesses++;
+                        Console.WriteLine("Det riktiga talet är större än ditt tal");            // if the user guesses a number thats lower than the actual number 
+                        numberOfGuesses++;                                                      //this will print out, and also add to the count     
+                        
                     }
                     else if (usersGuess > number)
                     {
-                        Console.WriteLine("Det riktiga talet är mindre än ditt tal");
-                        numberOfGuesses++;
+                        Console.WriteLine("Det riktiga talet är mindre än ditt tal");           // if the user guesses a number thats higher than the actual number 
+                        numberOfGuesses++;                                                      //this will print out, and also add to the count
                     }
-                    else if (usersGuess == number)
+                    else if (usersGuess == number)                                              // if the user guesses the correct number this will print out
                     {
                         Console.WriteLine("Grattis du valde rätt nummer");
                         break;
                     }
                 }
                 else
-                {
+                {                                                                               //just to add a safety measure to see if the doesnt user insert a number
                     Console.WriteLine("Var vänlig skriv in ett heltal.");
                 }
             }
-            if (numberOfGuesses == 5)
-            {
+            if (numberOfGuesses == 5)                                                           // if the user has used 5 tries and still hasnt figured it out
+            {                                                                                   // this will print out, with the correct number in mind
                 Console.WriteLine($"Du har använt dig av dina 5 chanser, det rätta svaret var {number}.");
 
             }
